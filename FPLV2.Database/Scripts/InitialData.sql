72648031b80b124710a656316b136ec3b2d844bd
Gary Fung <xzblaze@hotmail.com>
2023-03-29 22:21:59 +1300

    Update scripts

--- FPLV2.Database/Scripts/InitialData.sql
+++ FPLV2.Database/Scripts/InitialData.sql
@@ -1,2 +1,5 @@
-INSERT INTO [dbo].[stats] VALUES (1, 'Most Points In A Gameweek', 'Gameweek Dominator', '1_mostpointsinagameweek', 1, 1)
+IF ((SELECT COUNT(1) FROM [dbo].[stats]) = 0)
+BEGIN
+	INSERT INTO [dbo].[stats] VALUES (1, 'Most Points In A Gameweek', 'Gameweek Dominator', '1_mostpointsinagameweek', 1, 1)
+END
 GO
